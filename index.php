735fac65f0a596a547bbc75ea80900e2390b69d3
haspork <kondratev.pach@mail.ru>
2022-04-14 22:01:46 +0300

    calculation logic completed

---
+++ index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP-hw4</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <main class="main">
+        <form class="calc" method="POST">
+            <input type="text" class="calc__input" readonly name="calc">
+            <input type="button" class="calc__button calc__button_one" value="1">
+            <input type="button" class="calc__button calc__button_two" value="2">
+            <input type="button" class="calc__button calc__button_three" value="3">
+            <input type="button" class="calc__button calc__button_four" value="4">
+            <input type="button" class="calc__button calc__button_five" value="5">
+            <input type="button" class="calc__button calc__button_six" value="6">
+            <input type="button" class="calc__button calc__button_seven" value="7">
+            <input type="button" class="calc__button calc__button_eight" value="8">
+            <input type="button" class="calc__button calc__button_nine" value="9">
+            <input type="button" class="calc__button calc__button_zero" value="0">
+            <input type="button" class="calc__button calc__button_bracket-open" value="(">
+            <input type="button" class="calc__button calc__button_bracket-close" value=")">
+            <input type="button" class="calc__button calc__button_add" value="+">
+            <input type="button" class="calc__button calc__button_sub" value="-">
+            <input type="button" class="calc__button calc__button_mult" value="*">
+            <input type="button" class="calc__button calc__button_div" value="/">
+            <input type="button" class="calc__button calc__button_reset" value="c">
+            <input type="button" class="calc__button calc__button_delete" value="←">
+            <input type="submit" class="calc__button calc__button_result" value="=">
+        </form>
+    </main>
+    <?php
+        $res = '';
+        if (!empty($_POST["calc"])) {
+            $elems = explode(' ', $_POST["calc"]);
+
+            $keyBracketsOpen = [];
+
+            while (array_search('(', $elems) != false and array_search(')', $elems) != false) {
+                for ($i = 0; $i < count($elems); $i++) {
+                    if ($elems[$i] == '(') {
+                        array_push($keyBracketsOpen, $i);
+                    } elseif ($elems[$i] == ')') {
+                        $first = array_pop($keyBracketsOpen);
+                        $second = $i;
+                        $replacement = calc(array_slice($elems, $first + 1, $second - $first - 1));
+                        array_splice($elems, $first, $second - $first + 1, $replacement);
+                    }
+                }
+                
+            }
+            $res = calc(array_values(array_diff($elems, array(''))))[0];
+
+        }
+
+
+        function calc($arr) {
+            for ($i = 0; $i < count($arr); $i++) {
+                if ($arr[$i] == '*' or $arr[$i] == '/') {
+                    switch ($arr[$i]) {
+                        case '*':
+                            $arr[$i - 1] = (float)$arr[$i - 1] * (float)$arr[$i + 1];
+                            array_splice($arr, $i, 2);
+                            break;
+                        case '/':
+                            $arr[$i - 1] = (float)$arr[$i - 1] / (float)$arr[$i + 1];
+                            array_splice($arr, $i, 2);
+                            break;
+                    }
+                }
+            }
+
+            for ($i = 0; $i < count($arr); $i++) {
+                if ($arr[$i] == '+' or $arr[$i] == '-') {
+                    switch ($arr[$i]) {
+                        case '+':
+                            $arr[$i - 1] = (float)$arr[$i - 1] + (float)$arr[$i + 1];
+                            array_splice($arr, $i, 2);
+                            break;
+                        case '-':
+                            $arr[$i - 1] = (float)$arr[$i - 1] - (float)$arr[$i + 1];
+                            array_splice($arr, $i, 2);
+                            break;
+                    }
+                }
+            }
+
+            while (array_search('+', $arr) != false ||
+                   array_search('-', $arr) != false ||
+                   array_search('*', $arr) != false ||
+                   array_search('/', $arr) != false) {
+                    $arr =  calc($arr);
+            }    
+        
+            return $arr;
+        }
+    ?>
+    <script>
+        var res = '<?= $res ?>';
+    </script>
+    <script src="scripts/scripts.js"></script>
+</body>
+</html>
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,9 +2,15 @@ const btns = document.querySelectorAll(".calc__button");
 const btnValues = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '(', ')', '+', '-', '*', '/'];
 const input = document.querySelector(".calc__input");
 
+input.value = res;
+
 for (let i = 0; i < btns.length - 2; i++) {
     btns[i].addEventListener("click", function() {
-        input.value += btnValues[i];
+        if ('+-/*()'.includes(btnValues[i]) || '+-/*()'.includes(input.value[input.value.length - 1])) {
+            input.value += ' ' + btnValues[i];
+        } else {
+            input.value += btnValues[i];
+        }
     })
 }
 
@@ -13,5 +19,10 @@ btns[btns.length - 3].addEventListener("click", () => {
 })
 
 btns[btns.length - 2].addEventListener("click", () => {
-    input.value = input.value.slice(0, -1);
+    if (input.value[input.value.length - 2] == ' ') {
+        input.value = input.value.slice(0, -2);
+    } else {
+        input.value = input.value.slice(0, -1);
+    }
+    
 })
